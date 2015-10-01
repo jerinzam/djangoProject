44c629b1ff1497f48ac1398cfe51a9bd5bd1d739
jerinzam <jerinzam@gmail.com>
2015-10-02 05:04:11 +0530

    collect static

--- ground/settings.py
+++ ground/settings.py
@@ -166,4 +166,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = "/home/jerin/djangoProject/mysite.com/sitestatic"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
